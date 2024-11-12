22f04e3648add2b8851e4f2c9a971bb74ef3540e
zbbszn <nb3629@nyu.edu>
2024-11-12 16:34:23 -0500

    added button pin

--- testhub.ino
+++ testhub.ino
@@ -1,18 +1,22 @@
 int ledPin = 13;
 int ledPin2 = 14;
+int buttonPin1 = 33;
 
 void setup() {
   // put your setup code here, to run once:
   pinMode(ledPin, OUTPUT);
+  pinMode(buttonPin1, INPUT);
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  digitalWrite(ledPin, HIGH);
-  digitalWrite(ledPin2, HIGH);
-  delay(500);
-  digitalWrite(ledPin, LOW);
-  digitalWrite(ledPin2, LOW);
-  delay(500);
+  if (digitalRead(buttonPin1) == HIGH){
+    digitalWrite(ledPin, HIGH);
+    digitalWrite(ledPin2, HIGH);
+    delay(500);
+    digitalWrite(ledPin, LOW);
+    digitalWrite(ledPin2, LOW);
+    delay(500);
+  }
 }
