44f1e091b735a3f9f7a14414f2d0abcf12327ab0
zbbszn <nb3629@nyu.edu>
2024-11-12 16:32:15 -0500

    leds blink

--- testhub.ino
+++ testhub.ino
@@ -9,5 +9,10 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  digitalWrite(ledPin, HIGH);
+  digitalWrite(ledPin2, HIGH);
+  delay(500);
+  digitalWrite(ledPin, LOW);
+  digitalWrite(ledPin2, LOW);
+  delay(500);
 }
